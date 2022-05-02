a82ce0865342ef9e3ea8daa5ae67fdc5646dcdd1
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-05-02 18:40:00 +0000

    feat: add API endpoints

---
+++ src/ml/inference.py
@@ -0,0 +1,8 @@
+"""ML inference module"""
+import numpy as np
+
+def predict_location(image_data):
+    """Predict location from image"""
+    return np.array([0.0, 0.0])
+# Modified 2025-09-26
+# Modified 2025-09-25
