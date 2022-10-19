183d3afbea392fc6b9d03a6e9a1f38d2d3e51fbf
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-10-19 18:48:10 +0000

    feat: add core functionality

---
+++ tests/test_core.py
@@ -0,0 +1,11 @@
+"""Core functionality tests"""
+import pytest
+
+def test_basic():
+    assert True
+
+def test_location_validation():
+    from src.utils.helpers import validate_coordinates
+    assert validate_coordinates(0, 0) == True
+# Modified 2025-08-21
+# Modified 2025-10-10
