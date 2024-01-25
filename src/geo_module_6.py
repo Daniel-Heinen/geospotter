391cece0158d2cb3575f25cc1a62fc994977980e
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2024-01-25 09:39:59 +0000

    test: add unit tests

--- src/geo_module_6.py
+++ src/geo_module_6.py
@@ -11,3 +11,4 @@ class LocationAnalyzer6:
         result = self.model(torch.tensor(data))
         return {"lat": float(result[0]), "lng": float(result[1])}
 # Modified 2024-06-24
+# Modified 2024-01-25
